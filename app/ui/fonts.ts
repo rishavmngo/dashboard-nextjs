29f9f8ff74a176ed4239bb1df153a27803b135fa
rishavmngo <rishavinmngo@gmail.com>
2024-12-21 22:45:30 +0530

    added routes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["700", "400"], });
+
+export const inter = Inter({ subsets: ['latin'] });
